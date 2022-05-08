13812c3598fb1bef9a241145ba60cf6c75556f3a
JEALG <jalain.gosse@icloud.com>
2022-05-08 09:36:49 +0200

    autorisation modification liste type et sous type

--- desktop/js/alexaamazonmusic.js
+++ desktop/js/alexaamazonmusic.js
@@ -285,8 +285,8 @@ function addCmdToTable(_cmd)
   	tr += '</select>'
   	tr += '</td>'
   	tr += '<td>'
-    tr += '<span class="type disabled" type="' + init(_cmd.type) + '" disabled = "false">' + jeedom.cmd.availableType() + '</span>'
-    tr += '<span class="subType disabled" subType="' + init(_cmd.subType) + '"></span>'
+    tr += '<span class="type" type="' + init(_cmd.type) + '" disabled = "false">' + jeedom.cmd.availableType() + '</span>'
+    tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>'
     
     if (init(_cmd.type) == 'action' && init(_cmd.logicalId) != 'refresh') {
         tr += '<td>'
